5b408c1097855199657e7e22c543136620f2fb47
Spook <spook.devv@gmail.com>
2024-04-08 18:35:00 -0400

    Adding general layout

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./EventItem.module.css";
 import Button from "../ui/Button";
-import DateIcon from "../icons/icons/date-icon";
-import AddressIcon from "../icons/icons/address-icon";
-import ArrowRightIcon from "../icons/icons/arrow-right-icon";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
