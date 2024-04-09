62749048448f4a5ef2c462293ebb6aef5f4219bb
Spook <spook.devv@gmail.com>
2024-04-09 09:25:33 -0400

    Navigating to the filtered events page programatically

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import EventList from "../components/events/EventList";
 import { getFeaturedEvents } from "../dummy-data";
 import EventsSearch from "../components/events/EventsSearch";
+import { useRouter } from "next/router";
 
 function HomePage() {
   const featuredEvents = getFeaturedEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
   return (
     <div>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList events={featuredEvents} />
     </div>
   );
